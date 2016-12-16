1e3c02d5f3742829d0ed09ec57397b445f52c3d4
Mukunda Bharatheesha <M.Bharatheesha@tudelft.nl>
2016-12-16 11:32:28 +0100

    Fix config.sh for first installation.

--- script/installation/config.sh
+++ script/installation/config.sh
@@ -3,6 +3,9 @@ export PKG_CONFIG_PATH=$DEVEL_DIR/install/lib/pkgconfig:/opt/ros/indigo/lib/pkgc
 export PYTHONPATH=$DEVEL_DIR/install/lib/python2.7/site-packages:$DEVEL_DIR/install/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$DEVEL_DIR/install/lib:/opt/ros/indigo/lib:usr/local/lib:usr/install/lib:$LD_LIBRARY_PATH
 
-source /opt/ros/indigo/setup.bash
-source $DEVEL_DIR/install/setup.bash
+if [ -f $DEVEL_DIR/install/setup.bash ]; then
+   source $DEVEL_DIR/install/setup.bash
+else
+   source /opt/ros/indigo/setup.bash
+fi
 export ROS_PACKAGE_PATH=$DEVEL_DIR/src:$DEVEL_DIR/install/lib:$ROS_PACKAGE_PATH
