d4a718b84f6cc1f0cebaefea5e40bc985cd9e170
Mukunda Bharatheesha <M.Bharatheesha@tudelft.nl>
2016-12-20 12:38:00 +0100

    Update config.sh to avoid package path conflicts.

--- script/installation/config.sh
+++ script/installation/config.sh
@@ -3,9 +3,9 @@ export PKG_CONFIG_PATH=$DEVEL_DIR/install/lib/pkgconfig:/opt/ros/indigo/lib/pkgc
 export PYTHONPATH=$DEVEL_DIR/install/lib/python2.7/site-packages:$DEVEL_DIR/install/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$DEVEL_DIR/install/lib:/opt/ros/indigo/lib:usr/local/lib:usr/install/lib:$LD_LIBRARY_PATH
 
-if [ -f $DEVEL_DIR/install/setup.bash ]; then
-   source $DEVEL_DIR/install/setup.bash
+if [ -f $DEVEL_DIR/install/setup.zsh ]; then
+   source $DEVEL_DIR/install/setup.zsh
 else
-   source /opt/ros/indigo/setup.bash
+   source /opt/ros/indigo/setup.zsh
 fi
-export ROS_PACKAGE_PATH=$DEVEL_DIR/src:$DEVEL_DIR/install/lib:$ROS_PACKAGE_PATH
+#export ROS_PACKAGE_PATH=$DEVEL_DIR/src:$DEVEL_DIR/install/lib:$ROS_PACKAGE_PATH
